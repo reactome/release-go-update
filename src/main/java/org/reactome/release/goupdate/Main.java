a08ea195f68e1a952503a6a0bd3f82930bd1f3db
Joel Weiser <joel.weiser@oicr.on.ca>
2026-07-30 16:13:30 -0400

    removes unneeded SQLException from JavaDoc

--- src/main/java/org/reactome/release/goupdate/Main.java
+++ src/main/java/org/reactome/release/goupdate/Main.java
@@ -1,21 +1,17 @@
 package org.reactome.release.goupdate;
 
 import java.io.FileInputStream;
-import java.sql.SQLException;
 import java.util.Properties;
 
 /**
  * @author sshorser
  *
  */
-public class Main
-{
+public class Main {
 	/**
 	 * @param args
-	 * @throws SQLException 
 	 */
 	public static void main(String[] args) {
-
 		GoUpdateStep step = new GoUpdateStep();
 		String pathToResources = args.length > 0 ? args[0] : "./go-update.properties";
 
